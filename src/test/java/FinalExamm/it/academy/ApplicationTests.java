1e2f5de65760ad600b7211ad0c00a74adce75c73
Karina-YikesForever <karinakokosova8@gmail.com>
2020-06-22 20:53:28 +0600

    Initial commit

---
+++ src/test/java/FinalExamm/it/academy/ApplicationTests.java
@@ -0,0 +1,13 @@
+package FinalExamm.it.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
